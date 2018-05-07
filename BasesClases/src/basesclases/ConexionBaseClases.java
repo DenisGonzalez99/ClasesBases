4473bbe64d24836b64551f745bcada150230a72f
Denis Gonzalez Corrales <denis.gonzalez@cesjuanpablosegundo.es>
2018-05-07 13:56:19 +0200

    VERSION 2

--- BasesClases/src/basesclases/ConexionBaseClases.java
+++ BasesClases/src/basesclases/ConexionBaseClases.java
@@ -1,9 +1,12 @@
 package basesclases;
 
-
+import java.sql.Connection;
 import java.sql.Date;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -11,25 +14,30 @@ import java.util.logging.Logger;
 public class ConexionBaseClases {
 
     public static void main(String[] args) throws SQLException {
-                Scanner sc = new Scanner(System.in);
-                //Departamento
-                int dept_no;
-                String dnombre;
-                String loc;
-                //Empleado
-                int emp_no;
-                String apellido;
-                String oficio;       
-                int dir;
-                Date fecha_alt;
-                double salario;
-                double comision;
-                int dept_no_em;
-                
-       Departamento departamento = new Departamento();
-       // departamento.setDept_no(1);
-       // departamento.setDnombre("Denis");
-       // departamento.setLoc("Madrid");
+        Scanner sc = new Scanner(System.in);
+        //Departamento
+        int dept_no;
+        String dnombre;
+        String loc;
+        //Empleado
+        int emp_no;
+        String apellido;
+        String oficio;
+        int dir;
+        Date fecha_alt;
+        double salario;
+        double comision;
+        int dept_no_em;
+
+        Departamento departamento = new Departamento();
+        Empleado empleado = new Empleado();
+        Connection conexion;
+
+        Statement sentencia = null;
+
+        // departamento.setDept_no(1);
+        // departamento.setDnombre("Denis");
+        // departamento.setLoc("Madrid");
         /*
         Empleado empleado = new Empleado();
         empleado.setEmp_no(1);
@@ -41,125 +49,148 @@ public class ConexionBaseClases {
         empleado.setSalario(1200);
         empleado.setComision(1);
         empleado.setDept_no(10);
-        */
-
+         */
         Departamentos deps = new Departamentos();
         Empleados emps = new Empleados();
-        /*
+
         try {
-            
-          // deps.Create(departamento);
-        //  deps.Read("Denis");
-          //  deps.Update(1, departamento);
-            //deps.Delete(1);
-         //emps.Create(empleado);
-         // emps.Read("nombre");
-       //  emps.Update(1, empleado);
-       //emps.Delete(1);
-        } catch(SQLException ex){
-           System.out.println("Error al insertar: "+ex.getSQLState() + " "+ ex.getMessage());
-       }   finally{
-       try{
-           deps.Close();
-          } catch(SQLException ex){
-              System.out.println("Error al cerrar BBDD: "+ex.getSQLState()+ " "+ ex.getMessage());
-          }
+                         
+            int opcion;
+            boolean seguir = true;
+            int opcion1;
+            while (seguir) {
+
+                System.out.println("<--------------------------------------------------------------->");
+                System.out.println("<|-------------------------------------------------------------|>");
+                System.out.println("||A continuacion te voy a dar las opciones que pues escoger    ||");
+                System.out.println("||-------------------------------------------------------------||");
+                System.out.println("||Pulsa (1) si quieres Listar departamentos                    ||");    //FUNCIONA
+                System.out.println("||Pulsa (2) si quieres Listar empleados                        ||");
+                System.out.println("||Pulsa (3) si quires buscar un empleado por su nombre         ||");
+                System.out.println("||Pulsa (4) si quieres buscar un departamento por su nombre    ||");
+                System.out.println("||Pulsa (5) si quieres crear un empleado                       ||");
+                System.out.println("||Pulsa (6) si quieres crear un departamento                   ||");  //FUNCIONA
+                System.out.println("||Pulsa (7) si quieres borrar un empleado                      ||");  //FUNCIONA
+                System.out.println("||Pulsa (8) si quieres borrar un departamento                  ||");  //FUNCIONA
+                System.out.println("||Pulsa (0) si quieres salir                                   ||");  //FUNCIONA
+                System.out.println("<|-------------------------------------------------------------|>");
+                System.out.println("<--------------------------------------------------------------->");
+
+                opcion = sc.nextInt();
+
+                switch (opcion) {
+                    case 1:                 //
+                        System.out.println("<--------------------------------------------------------------->");
+                        System.out.println("<|-------------------------------------------------------------|>");
+                        System.out.println("||A continuacion te voy a dar las opciones que pues escoger    ||");        
+                        System.out.println("||-------------------------------------------------------------||");
+                        System.out.println("||Pulsa (1) si quieres listar un departamento                  ||");        //funciona
+                        System.out.println("||Pulsa (2) si quieres Listar tosdos los departamentos         ||");        //funciona
+                        System.out.println("<|-------------------------------------------------------------|>");
+                        System.out.println("<--------------------------------------------------------------->");
+                        opcion1 = sc.nextInt();
+                        switch (opcion1) {
+
+                            case 1:         //FUNCIONA
+                                System.out.println("Introduce el numero del departamento que quieras buscar");
+                                dept_no = sc.nextInt();
+//                departamento.setDept_no(dept_no);
+                                departamento = deps.ReadNum(dept_no);
+                                // imprimo departamento
+                                System.out.printf("+--------------------------------------------------+ %n");
+                                System.out.printf("|%-10s |%-20s |%-15s |%n", "Dpt_no", "Nom.Dep", "Ciudad");
+                                System.out.printf("+--------------------------------------------------+ %n");
+
+                                System.out.printf("|%10d |%-20s |%-15s |%n", departamento.getDept_no(),departamento.getDnombre(),departamento.getLoc());
+                                System.out.printf("+--------------------------------------------------+ %n");
+                                break;
+
+                            case 2:     //FUNCIONA
+                                ArrayList<Departamento> departamentosAL = new ArrayList<>();
+                                departamentosAL = deps.ReadTodos();
+                                System.out.printf("+--------------------------------------------------+ %n");
+                                System.out.printf("|%-10s |%-20s |%-15s |%n", "Dpt_no", "Nom.Dep", "Ciudad");
+                                System.out.printf("+--------------------------------------------------+ %n");
+                                for (int i = 0; i < departamentosAL.size(); i++) {
+                                    System.out.printf("|%10d |%-20s |%-15s |%n", departamentosAL.get(i).getDept_no(), departamentosAL.get(i).getDnombre(), departamentosAL.get(i).getLoc());
+                                }
+                                System.out.printf("+--------------------------------------------------+ %n");
+                                break;
+                        }
+
+                    case 2:
+                        //
+                        break;
+                    case 3:
+
+                        System.out.println("Dinos el nombre del departamento");
+
+                        break;
+                    case 4:
+                        System.out.println("Dinos el nombre del departamento");
+                         dnombre = sc.nextLine();
+                         
+
+                        deps.Read(dnombre);
+
+                                System.out.printf("+--------------------------------------------------+ %n");
+                                System.out.printf("|%-10s |%-20s |%-15s |%n", "Dpt_no", "Nom.Dep", "Ciudad");
+                                System.out.printf("+--------------------------------------------------+ %n");
+
+                                System.out.printf("|%10d |%-20s |%-15s |%n", departamento.getDept_no(),departamento.getDnombre(),departamento.getLoc());
+                                System.out.printf("+--------------------------------------------------+ %n");
+                        break;
+                    case 5:
+                        //
+                        break;
+                    case 6:                     //FUNCIONA
+                        System.out.println("Vamos a crear un departamento");
+                        System.out.println("Mete cada dato según te digamos, y despues de meter cada dato presiona Enter");
+                        System.out.println("Primero el numero del departamento, luego dejas un espacio y pones el nombre del departamento por ultimo pones una ',' y pones la localización");
+
+                        dept_no = sc.nextInt();
+                        departamento.setDept_no(dept_no);
+
+                        String string = sc.nextLine();
+                        String[] partss = string.split(",");
+                        String part11 = partss[0]; // dnombre
+                        String part2 = partss[1]; // loc                      
+                        departamento.setDnombre(partss[0]);
+                        departamento.setLoc(partss[1]);
+
+                        deps.Create(departamento);
+
+                        break;
+
+                    case 7:                                             //FUNCIONA
+                        System.out.println("Dinos el numero del empleado que quieres borrar");
+                        emp_no = sc.nextInt();
+                        emps.Delete(emp_no);
+                        break;
+
+                    case 8:                     //FUNCIONA
+                        System.out.println("Dinos el numero del departamento que quieres borrar");
+                        dept_no = sc.nextInt();
+                        deps.Delete(dept_no);
+
+                        break;
+                    case 0:            //FUNCIONA
+                        seguir = false;
+                        break;
+
+                }
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error al insertar: " + ex.getSQLState() + " " + ex.getMessage());
+        } finally {
+            try {
+                deps.Close();
+            } catch (SQLException ex) {
+                System.out.println("Error al cerrar BBDD: " + ex.getSQLState() + " " + ex.getMessage());
+            }
         }
-        */
-        
-        int opcion;
-        boolean seguir = true;
-        
-        while(seguir){
-            
-        System.out.println("<------------------------------------------------------------>");
-        System.out.println("<------------------------------------------------------------>");
-        System.out.println("|A continuacion te voy a dar las opciones que pues escoger    |");
-        System.out.println("|-------------------------------------------------------------|");
-        System.out.println("|Pulsa (1) si quieres Listar departamentos                    |");
-        System.out.println("|Pulsa (2) si quieres Listar empleados                        |");
-        System.out.println("|Pulsa (3) si quires buscar un empleado por su nombre         |");
-        System.out.println("|Pulsa (4) si quieres buscar un departamento por su nombre    |");
-        System.out.println("|Pulsa (5) si quieres crear un empleado                       |");
-        System.out.println("|Pulsa (6) si quieres crear un departamento                   |");  //FUNCIONA
-        System.out.println("|Pulsa (7) si quieres borrar un empleado                      |");  //FUNCIONA
-        System.out.println("|Pulsa (8) si quieres borrar un departamento                  |");  //FUNCIONA
-        System.out.println("|Pulsa (0) si quieres salir                                   |");  //FUNCIONA
-        System.out.println("<------------------------------------------------------------->");
-        System.out.println("<------------------------------------------------------------->");
 
-        
-       opcion = sc.nextInt();
-
-       
-       switch(opcion){
-	case 1:
-		
-            //
-		break; 
-	case 2:
-		//
-            break;
-        case 3:
-            
-            System.out.println("Dinos el nombre del departamento");
-
-            
-            break;
-        case 4:
-            System.out.println("Dinos el nombre del departamento");
-            String strings = sc.nextLine();
-            String[] parts = strings.split("");
-            String part1 = parts[0]; // dnombre
-            
-            deps.Read(parts[0]);                 
-            
-            break;
-        case 5:
-            //
-            break;
-        case 6:                     //FUNCIONA
-            System.out.println("Vamos a crear un departamento");
-            System.out.println("Mete cada dato según te digamos, y despues de meter cada dato presiona Enter");
-            System.out.println("Primero el numero del departamento, luego dejas un espacio y pones el nombre del departamento por ultimo pones una ',' y pones la localización");
-
- 
-            dept_no = sc.nextInt();
-            departamento.setDept_no(dept_no);
-            
-            String string = sc.nextLine();
-            String[] partss = string.split(",");
-            String part11 = partss[0]; // dnombre
-            String part2 = partss[1]; // loc                      
-            departamento.setDnombre(partss[0]);
-            departamento.setLoc(partss[1]);
-
-            deps.Create(departamento);
-
-            break;
-            
-        case 7:                                             //FUNCIONA
-            System.out.println("Dinos el numero del empleado que quieres borrar");
-            emp_no = sc.nextInt();
-            emps.Delete(emp_no);
-            break;
-            
-        case 8:                     //FUNCIONA
-        System.out.println("Dinos el numero del departamento que quieres borrar");
-        dept_no = sc.nextInt();        
-        deps.Delete(dept_no);
-
-            break;
-        case 0:            //FUNCIONA
-            seguir = false;
-            break;
-       
-       
-
-        }
     }
-
-}
 }/*Crear un programa que presente las siguientes opciones:
 Listar Departamentos. Aquí debe dar la opción de listar un solo departamento o todos.
 Listar Empleados. Aquí debe dar la opción de listar un solo empleado o todos.
@@ -169,4 +200,4 @@ Crear empleado
 Crear departamento
 Borrar empleado
 Borrar departamento
-*/
+ */
--- BasesClases/src/basesclases/Departamentos.java
+++ BasesClases/src/basesclases/Departamentos.java
@@ -15,7 +15,7 @@ public class Departamentos {
 
     private Connection conexion;
     private ArrayList<Departamento> departamentos;
-                Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
 
     Statement sentencia = null;
 
@@ -32,90 +32,114 @@ public class Departamentos {
     public int Create(Departamento dep) throws SQLException {
 
         int filas = 0;
-        String sql = "INSERT INTO departamentos VALUES"+ " ( ?, ?, ? )";
+        String sql = "INSERT INTO departamentos VALUES" + " ( ?, ?, ? )";
         PreparedStatement sentencia;
-       
-            sentencia = conexion.prepareStatement(sql);
-            
-            sentencia.setInt(1, dep.getDept_no());
-            sentencia.setString(2, dep.getDnombre());
-            sentencia.setString(3, dep.getLoc());
-            
 
-            
-                 //       System.out.printf("|%-5s |%-10s |%-15s |%-10s |%n","No", "Apellido", "Nom.Dep", "Ciudad");
+        sentencia = conexion.prepareStatement(sql);
 
-            
-            filas = sentencia.executeUpdate();
-           return filas;
-            
+        sentencia.setInt(1, dep.getDept_no());
+        sentencia.setString(2, dep.getDnombre());
+        sentencia.setString(3, dep.getLoc());
+
+        //       System.out.printf("|%-5s |%-10s |%-15s |%-10s |%n","No", "Apellido", "Nom.Dep", "Ciudad");
+        filas = sentencia.executeUpdate();
+        return filas;
+
     }
 
     public String Update(int dept_no, Departamento dep) throws SQLException {
 
-        
+        String sql = "UPDATE departamentos set loc = 'cuenca' where dept_no= " + dept_no;
 
-        String sql = "UPDATE departamentos set loc = 'Cuenca' where dept_no= "+dept_no ;
+        sentencia = conexion.createStatement();
 
-        
-            sentencia = conexion.createStatement();
+        sentencia.execute(sql);
 
-            sentencia.execute(sql);
-
-            ResultSet rs = sentencia.getResultSet();
-            
+        ResultSet rs = sentencia.getResultSet();
 
         //    rs.close();
+        sentencia.close();
 
-            sentencia.close();
-
-            return sql;
-       
-       
+        return sql;
 
     }
 
     public Departamento Read(String dnombre) throws SQLException {
 
         Departamento dep = null;
-                            
 
+        String sql = "select * from departamentos where dnombre = '" +dnombre+"'";
 
-        String sql = "select * from departamentos where dnombre = "+dnombre;
-       
-            sentencia = conexion.createStatement();
-            sentencia.execute(sql);
+        sentencia = conexion.createStatement();
+        sentencia.execute(sql);
 
-            ResultSet rs = sentencia.getResultSet();  
+        ResultSet rs = sentencia.getResultSet();
 
-            rs.close();
+        while (rs.next()) {
+            dep = new Departamento(rs.getInt("dept_no"), rs.getString("dnombre"), rs.getString("loc"));
 
-            sentencia.close();
-       
+        }
+
+        sentencia.close();
 
         return dep;
 
     }
 
-    public void Delete(int dept_no) throws SQLException {
-        
-        
-        String sql = "delete from departamentos where dept_no = "+dept_no;
-        
+    public Departamento ReadNum(int dpt_no) throws SQLException {
+
+        Departamento dep = null;
+
+        String sql = "select * from departamentos where dept_no = " + dpt_no;
+
         sentencia = conexion.createStatement();
-            sentencia.execute(sql);
+        sentencia.execute(sql);
+        ResultSet rs = sentencia.getResultSet();
+        while (rs.next()) {
+            dep = new Departamento(rs.getInt("dept_no"), rs.getString("dnombre"), rs.getString("loc"));
 
-            ResultSet rs = sentencia.getResultSet();
+        }
 
-          //  rs.close();
+        sentencia.close();
 
-            sentencia.close();
-            
-           // return sql;
-        
-        
-        
+        return dep;
 
+    }
+
+    public ArrayList<Departamento> ReadTodos() throws SQLException {
+
+        Departamento dep = null;
+        ArrayList<Departamento> deps = new ArrayList<>();
+
+        String sql = "select * from departamentos";
+
+        sentencia = conexion.createStatement();
+        sentencia.execute(sql);
+        ResultSet rs = sentencia.getResultSet();
+        while (rs.next()) {
+            dep = new Departamento(rs.getInt("dept_no"), rs.getString("dnombre"), rs.getString("loc"));
+            deps.add(dep);
+        }
+        rs.close();
+        sentencia.close();
+
+        return deps;
+
+    }
+
+    public void Delete(int dept_no) throws SQLException {
+
+        String sql = "delete from departamentos where dept_no = " + dept_no;
+
+        sentencia = conexion.createStatement();
+        sentencia.execute(sql);
+
+        ResultSet rs = sentencia.getResultSet();
+
+        //  rs.close();
+        sentencia.close();
+
+        // return sql;
     }
 
     public void Close() throws SQLException {
